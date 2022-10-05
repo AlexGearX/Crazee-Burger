590c475317624d5a0b4bbb41392a7d5f314197dd
AlexGearX <alexgear34@gmail.com>
2022-10-05 11:09:24 +0200

    ✨ F01 : LOGIN > Add title and form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import { useState } from "react";
+import LoginPages from "./components/pages/LoginPage";
+
 function App() {
-
-
-  //state
-  const [prenom, setPrenom] = useState("");
-
-  // comportements
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("Bonjour " + prenom);
-    setPrenom("");
-  };
-
-  const handleChange = (event) => {
-    setPrenom(event.target.value);
-  }
-  //render
   return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-
-      <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input value={prenom} type="text" placeholder="Entrez votre prénom..." onChange={handleChange} required></input><button>Accéder à votre espace</button>
-      </form>
-    </div>
+    <LoginPages />
   );
 }
 
