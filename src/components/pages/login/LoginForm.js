67e091d821a1cab6e12f3a4e9846675ac12eb99c
AlexGearX <alexgear34@gmail.com>
2022-11-11 16:25:44 +0100

    💄 F03 : Add BackGround & Styled loginPage(WIP)

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { theme } from "../../../theme";
+import { FaRegHandPointer } from "react-icons/fa";
 
 export default function LoginForm() {
   const navigate = useNavigate();
@@ -12,32 +14,67 @@ export default function LoginForm() {
 
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input
-        value={name}
-        type="text"
-        placeholder="Entrez votre prénom..."
-        onChange={handleChange}
-        required
-      ></input>
-      <button>Accéder à votre espace</button>
+      <h1 className="h1-LoginForm">Bienvenue chez nous !</h1>
+      <div class="ligne"></div>
+      <h2 className="h2-LoginForm">Connectez-vous</h2>
+      <div className="cta-container">
+        <div className="input-container">
+          {/* <img src={FaRegHandPointer} alt="Person"></img> */}
+          <input
+            className="cta-LoginForm "
+            value={name}
+            type="text"
+            placeholder="Entrez votre prénom"
+            onChange={handleChange}
+            required
+          ></input>
+        </div>
+        <button className="cta-LoginForm ">Accéder à votre espace</button>
+      </div>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-  h1 {
-    font-family: "Amatic SC", cursive;
+  .h1-LoginForm {
+    font-size: ${theme.fonts.P5};
   }
-  h2 {
+  .ligne {
+    width: 450px;
+    border: 1px solid ${theme.colors.orange};
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .h2-LoginForm {
     font-family: "Amatic SC", cursive;
+    text-align: center;
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.P4};
+  }
+  .cta-container {
+    font-family: "Open Sans", sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .input-container {
+  }
+  .cta-LoginForm {
+    height: 50px;
+    border: 1px solid black;
+    border-radius: ${theme.borderRadius.round};
+    font-size: ${theme.fonts.P1};
+    font-weight: ${theme.weights.semiBold};
   }
   input {
-    font-family: "Open Sans", sans-serif;
+    width: 450px;
+    color: ${theme.colors.dark};
+    margin-bottom: 20px;
   }
   button {
-    font-family: "Open Sans", sans-serif;
+    width: 458px;
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.primary};
   }
 `;
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,13 +1,51 @@
 import styled from "styled-components";
 import LoginForm from "./LoginForm";
-export default function LoginPage() {
-    return (
-        <LoginPageStyled>
-            <LoginForm />
-        </LoginPageStyled>
-    );
+import logoLoginPage from "../../../assets/images/login/F03logo-orange.png";
+import background from "../../../assets/images/login/F03burger-background.jpg";
+import { theme } from "../../../theme";
 
-};
+export default function LoginPage() {
+  return (
+    <LoginPageStyled
+      style={{
+        backgroundColor: "black",
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        overflow: "hidden",
+      }}
+    >
+      <div className="LoginPageTitle">
+        <h1 className="h1-LoginPages">CRAZEE</h1>
+        <img src={logoLoginPage} alt="Logo" />
+        <h1 className="h1-LoginPages">BURGER</h1>
+      </div>
+      <LoginForm />
+    </LoginPageStyled>
+  );
+}
 const LoginPageStyled = styled.div`
-    border: 1px solid red;
-`
+  .LoginPageTitle {
+    margin-top: 100px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+  h1 {
+    font-family: "Amatic SC", cursive;
+    text-align: center;
+    color: ${theme.colors.white};
+  }
+
+  .h1-LoginPages {
+    color: ${theme.colors.primary};
+    font-size: ${theme.fonts.P6};
+    letter-spacing: ${theme.spacing.xxs};
+    font-weight: ${theme.weights.heavy};
+  }
+  img {
+    width: 200px;
+  }
+`;
