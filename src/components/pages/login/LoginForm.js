6bcc58625fc4a21adb7bc7c3c4b8c76e5a5c7c0a
AlexGearX <alexgear34@gmail.com>
2022-11-11 12:02:19 +0100

    🍱 F03 : Import font & images

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
 
-    const navigate = useNavigate();
-    const [name, setName] = useState("");
+  const handleSubmit = () => navigate(`/order/${name}`);
 
+  const handleChange = (event) => setName(event.target.value);
 
-    const handleSubmit = () => navigate(`/order/${name}`)
-
-    const handleChange = (event) => setName(event.target.value)
-
-
-    return (
-        <form action="submit" onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous !</h1>
-            <br />
-            <h2>Connectez-vous</h2>
-            <input
-                value={name}
-                type="text"
-                placeholder="Entrez votre prénom..."
-                onChange={handleChange}
-                required>
-            </input>
-            <button>Accéder à votre espace</button>
-        </form>
-    )
+  return (
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <input
+        value={name}
+        type="text"
+        placeholder="Entrez votre prénom..."
+        onChange={handleChange}
+        required
+      ></input>
+      <button>Accéder à votre espace</button>
+    </LoginFormStyled>
+  );
 }
+
+const LoginFormStyled = styled.form`
+  h1 {
+    font-family: "Amatic SC", cursive;
+  }
+  h2 {
+    font-family: "Amatic SC", cursive;
+  }
+  input {
+    font-family: "Open Sans", sans-serif;
+  }
+  button {
+    font-family: "Open Sans", sans-serif;
+  }
+`;
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,9 +1,13 @@
+import styled from "styled-components";
 import LoginForm from "./LoginForm";
-
 export default function LoginPage() {
     return (
-        <div>
+        <LoginPageStyled>
             <LoginForm />
-        </div>
+        </LoginPageStyled>
     );
+
 };
+const LoginPageStyled = styled.div`
+    border: 1px solid red;
+`
