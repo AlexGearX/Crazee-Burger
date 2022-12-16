7c81d362e244d2b76502d3ba7b38cdb627587a05
AlexGearX <alexgear34@gmail.com>
2022-12-17 00:54:44 +0100

    💄 F07:MAIN > display Basket

--- src/components/pages/order/main/Basket.js
+++ src/components/pages/order/main/Basket.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function Basket() {
-  return <BasketStyled>Basket</BasketStyled>;
+  return <BasketStyled></BasketStyled>;
 }
 const BasketStyled = styled.div`
-  background: Blue;
+  background: ${theme.colors.background_dark};
 `;
