1cc926150c82486ad57d68fb25cd4c797750e63b
AlexGearX <alexgear34@gmail.com>
2023-03-12 17:04:00 +0100

    💄 F07 : fix tab style on hover

--- src/components/reusable-ui/buttons/Tab.js
+++ src/components/reusable-ui/buttons/Tab.js
@@ -5,50 +5,48 @@ import { theme } from "../../../theme";
 export default function Tab({ onClick, label, icon, className }) {
   return (
     <TabStyled className={className} onClick={onClick}>
-      <div className="btn-container">
         {icon && <div className="icon">{icon}</div>}
-        {label && <p>{label}</p>}
-      </div>
+        {label && <span>{label}</span>}
     </TabStyled>
   );
 }
 
 const TabStyled = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-
-  font-family: "Open Sans", sans-serif;
-  font-size: ${theme.fonts.P0};
-  font-weight: ${theme.weights.regular};
-  background: ${theme.colors.white};
-  border: 1px solid ${theme.colors.greyLight};
-  border-radius: ${theme.borderRadius.round} ${theme.borderRadius.round} 0 0;
-  color: ${theme.colors.greyLightBlue};
-  
-  height: 43px;
+  border: 1px solid blue;
   padding: 0 22px;
-  margin-right: 1px;
-p{
-  padding-left:10px;
-}
-  .btn-container{
+  height: 43px;
+
   display: flex;
-  flex-direction: row;
+  justify-content: center;
   align-items: center;
-  }
 
   cursor: pointer;
 
-  :hover {
-    text-decoration: underline ${theme.colors.greyLightBlue};
-    border-bottom: 1px solid ${theme.colors.white};
-    background-color: ${theme.colors.white};
-  }
+  font-size: ${theme.fonts.P0};
+  color: ${theme.colors.greySemiDark};
+
+  position: relative;
+  left: 5%;
+  top: 1px;
+
+  background: #ffffff;
+  border-width: 1px 1px 2px 1px;
+  border-style: solid;
+  border-color: #e4e5e9;
+  box-shadow: ${theme.shadows.subtle};
+  border-radius: 5px 5px 0px 0px;
 
   .icon {
-    font-size: 16px;
-    padding: 5px 0 0 0;
+    display: flex;
   }
-`;
+
+  span {
+    margin-left: 13px;
+  }
+
+  :hover {
+    border-bottom: 2px solid ${theme.colors.white};
+    text-decoration: underline;
+  }
+`
+
