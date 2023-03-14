54d1b6a88816a6ad70d8f29ce76ffb1e08f6c0f0
AlexGearX <alexgear34@gmail.com>
2023-03-14 21:04:41 +0100

    🚧 F08: Admin > Addform : Set Layout

--- src/components/pages/order/main/Admin/Panel/AddForm.js
+++ src/components/pages/order/main/Admin/Panel/AddForm.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function AddForm() {
   return (
-    <div>Ajouter un produit</div>
+    <AddFormStyled>
+      <div className='image-preview'>Image Preview</div>
+      <div className='input-fields'>
+        <input type="text" placeholder="Nom du produit (ex: Super Burger)"></input>
+        <input type="text" placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"></input>
+        <input type="text" placeholder='Prix'></input>
+      </div>
+      <div className='submit-button'>Validation</div>  
+    </AddFormStyled> 
   )
 }
+
+const AddFormStyled = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width:70%;
+
+  .image-preview{
+    background: red;
+    grid-area: 1/1/4/2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .input-fields{
+    background: blue;
+    grid-area: 1/2/-2/-1;
+    display: grid;
+  }
+  .submit-button{ 
+    background: brown;
+    grid-area: 4/2/-1/-1;
+    width: 50%;
+  }
+`;
